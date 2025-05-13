d85c8d82718aeb8e702a7d2d05a78cb7b54678e5
Angel González <140091077+AngelSGonza2107@users.noreply.github.com>
2025-05-13 07:51:08 -0600

    Delete Fase 3/APIs/API reconocimiento facial/config.py

--- Fase 3/APIs/API reconocimiento facial/config.py
+++
@@ -1,7 +0,0 @@
-AWS_CREDENTIALS = {
-    'aws_access_key_id': 'AKIAYQYUATNTZL53SRFS',  # Reemplaza con tus credenciales
-    'aws_secret_access_key': '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',
-    'region_name': 'us-east-1'
-}
-
-COLLECTION_ID = "office-access-system"
